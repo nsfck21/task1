52333b53bd47d6ff4ee6bdfb2efcb15169a7b451
nsfck21 <nsfck21@gmail.com>
2025-11-26 12:22:26 +0530

    on Nov 26 2025- arithmatic operations

---
+++ src/maths.java
@@ -0,0 +1,11 @@
+public class maths {
+    public static void main(String[] args)
+    {
+        System.out.println(2+3);
+        System.out.println(10-5);
+        System.out.println(6/2);
+        System.out.println(10*5);
+        //System.out.println(10/0);// this is abnormal error in tht program
+    }
+
+}
