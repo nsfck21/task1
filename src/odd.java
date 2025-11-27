581de80b57075a70db0098fc90bdfb6726c31a52
nsfck21 <nsfck21@gmail.com>
2025-11-27 10:12:18 +0530

    on Nov 27 2025- check even or odd

---
+++ src/odd.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class odd {
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter a number");
+        int num= sc.nextInt();
+        if (num % 2 == 0)
+
+            System.out.println("even");
+        else
+            System.out.println("odd");
+
+
+
+
+
+
+    }
+
+}
+
+
+
